4a0ffb101fe3224774bd0f6471bbbd3f956f1864
Samridh Gupta <samridh@particle41.com>
2019-11-26 21:38:24 +0530

    REMOVED rn-payments

--- android/app/src/main/java/com/hiddenchalk/MainApplication.java
+++ android/app/src/main/java/com/hiddenchalk/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativepayments.ReactNativePaymentsPackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import com.aws.reactnatives3.RNAwsS3Package;
 import com.aws.reactnativecognito.RNAwsSdkPackage;
